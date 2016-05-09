ac50adcdfe404afa8bd5efaed2e7ea6c48fdc51e
藤本大輝 <d_fujimoto@ic-com.co.jp>
2016-05-09 17:58:04 +0900

    0509Kadai

--- UriageSyukei.java
+++ UriageSyukei.java
@@ -11,10 +11,11 @@ import java.util.regex.Pattern;
 
 public class UriageSyukei {
 	public static void main(String[] args) {
-		HashMap<String, Long> uriagesum = new HashMap<String, Long>(); //マップオブジェクト生成
+		HashMap<String, Long> brasum = new HashMap<String, Long>(); //マップオブジェクト生成
+		HashMap<String, Long> comsum = new HashMap<String, Long>(); //マップオブジェクト生成
 //1.支店定義ファイル読み込み
 		//if(args.length != 1)
-		HashMap<String, String> siten = new HashMap<String, String>(); //マップオブジェクト生成(名前)
+		HashMap<String, String> store = new HashMap<String, String>(); //マップオブジェクト生成(名前)
 		try {
 			File file = new File(args[0],"branch.lst");  //ファイル名を探す
 			FileReader filereader = new FileReader(file);  //ファイルから文字列をバッファへ渡す
@@ -25,8 +26,9 @@ public class UriageSyukei {
 			while ((line = br.readLine()) != null) {  //文字列データの受け取り
 				String[] items = line.split(",");  //カンマで分ける
 //				System.out.println(items[0]);
-				siten.put(items[0], items[1]);  //items[0]支店コード, items[1]支店名、を格納
-//				System.out.println(siten.get(items[0]));
+				store.put(items[0], items[1]);  //items[0]支店コード, items[1]支店名、を格納
+				brasum.put(items[0], 0L);
+//				System.out.println(store.get(items[0]));
 
 				for(int i =0; i < items.length; i++) {
 //					System.out.println(items[0] + "," + items[1]);
@@ -51,7 +53,7 @@ public class UriageSyukei {
 		System.out.println();
 
 //2.商品定義ファイル
-		HashMap<String, String> syohin = new HashMap<String, String>(); //マップオブジェクト生成
+		HashMap<String, String> products = new HashMap<String, String>(); //マップオブジェクト生成
 		try {
 			File file = new File(args[0],"commodity.lst");
 			FileReader filereader = new FileReader(file);
@@ -62,15 +64,16 @@ public class UriageSyukei {
 			while ((line = br.readLine()) != null) {  //文字列データの受け取り
 				String[] item = line.split(",");  //カンマで分ける
 
-				syohin.put(item[0], item[1]);  //item[0]商品コード, item[1]商品名、を格納
+				products.put(item[0], item[1]);  //item[0]商品コード, item[1]商品名、を格納
+				comsum.put(item[0], 0L);
 //				for(int i =0; i < items.length; i++) {
 //					System.out.println(item[0] + "," + item[1]);
-//					System.out.println(syohin.get(item[0]));
+//					System.out.println(products.get(item[0]));
 //				}
-				String str = item[0];
+//				String str = item[0];
 
 				Pattern p = Pattern.compile("^\\w{8}$");  //半角英数かつ数値 8桁と一致
-		        Matcher m = p.matcher(str);
+		        Matcher m = p.matcher(item[0]);
 				if(!m.find()) {
 					System.out.println("商品定義ファイルのフォーマットが不正です");
 				}
@@ -87,7 +90,7 @@ public class UriageSyukei {
 		File file = new File(args[0]);
 		File files[] = file.listFiles();  //ファイルの一覧をFile型の配列で返す
 
-		ArrayList<File> uriage = new ArrayList<File>();
+		ArrayList<File> sum = new ArrayList<File>();
 		for(int i = 0; i < files.length; i++) {
 			if(files[i].getName().endsWith(".rcd")) {  //接尾語が.rcdと一致
 				String[] item = files[i].getName().toString().split("\\.");  //[C:\Kadai]の表示なし、文字列を.で区切る
@@ -95,28 +98,54 @@ public class UriageSyukei {
 				int j = Integer.parseInt(item[0]);  //文字列を数値に変換
 
 				if(j - 1 == i) {
-				//System.out.println(j);
+//				System.out.println(j);
 				} else {
 					System.out.println("売上ファイル名が連番になっていません");
 					return;
 				}
-				uriage.add(files[i]);
+				sum.add(files[i]);
 			}
 		}
 		try {
-			for(int k = 0; k < uriage.size(); k++) {
-				File String = new File(uriage.get(k).toString());
+			for(int k = 0; k < sum.size(); k++) {
+				File String = new File(sum.get(k).toString());
 				FileReader filereader = new FileReader(String);
 				BufferedReader br = new BufferedReader(filereader);
 				String line;
 
-				ArrayList<String> nakami = new ArrayList<String>();
+				ArrayList<String> contents = new ArrayList<String>(); //リストに、rcdファイルのみを格納
 				while ((line = br.readLine()) != null) {  //文字列データの受け取り
-					nakami.add(line);
+					contents.add(line);
 				}
-//				System.out.println(nakami.get(0));
-//				System.out.println(nakami.get(1));
-//				System.out.println(nakami.get(2));
+//				System.out.println(contents.get(0)); //支店番号
+//				System.out.println(contents.get(1)); //商品コード
+//				System.out.println(contents.get(2)); //売上額
+
+				long sale = Long.parseLong(contents.get(2)); //売上げ額をLong型の数値に
+				long shop = brasum.get(contents.get(0)); //支店番号をgetする
+				long code = comsum.get(contents.get(1)); //商品コードをgetする
+//				System.out.println(contents.get(0));
+//				System.out.println(sale + shop);
+
+				brasum.put(contents.get(0), sale + shop); //支店ごとの売上合計をマップにいれる
+				comsum.put(contents.get(1), sale + code); //商品ごとの売上合計をマップに入れる
+
+				String str = Long.toString(sale + shop);
+
+				Pattern p = Pattern.compile("^\\d{1,10}$"); //1～10桁までの半角英数にマッチするか
+				Matcher m = p.matcher(str);
+				if(!m.find()) {
+					System.out.println("合計金額が10桁を超えました");
+					return;
+				}
+
+//				String shopM = Long.toString(shop);
+//				if(Pattern.compile(shopM).matcher().matches()) {
+//				}
+
+				if(!(contents.size() == 3)) { //要素数が3と同じではない場合のみ、以下のメッセージを表示
+				System.out.println("＜該当ファイル名＞のフォーマットが不正です");
+				}
 
 				br.close();
 				filereader.close();
@@ -128,6 +157,7 @@ public class UriageSyukei {
 				System.out.println("商品定義ファイルのフォーマットが不正です");
 				return;
 		}
-//		System.out.println(uriage);
+//		System.out.println(brasum.entrySet());
+//		System.out.println(comsum.entrySet());
 	}
 }
