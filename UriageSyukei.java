62e7b07139d43448a976a03b3353d89c0fa79133
藤本大輝 <d_fujimoto@ic-com.co.jp>
2016-05-10 17:33:43 +0900

    0510Kadai

--- UriageSyukei.java
+++ UriageSyukei.java
@@ -1,10 +1,17 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,9 +33,10 @@ public class UriageSyukei {
 			while ((line = br.readLine()) != null) {  //文字列データの受け取り
 				String[] items = line.split(",");  //カンマで分ける
 //				System.out.println(items[0]);
-				store.put(items[0], items[1]);  //items[0]支店コード, items[1]支店名、を格納
+				store.put(items[0], items[1]);  //items[0]支店コードをキーに, items[1]支店名、を格納
 				brasum.put(items[0], 0L);
 //				System.out.println(store.get(items[0]));
+//				System.out.println(brasum.get(items[0]));
 
 				for(int i =0; i < items.length; i++) {
 //					System.out.println(items[0] + "," + items[1]);
@@ -39,6 +47,7 @@ public class UriageSyukei {
 		        Matcher m = p.matcher(str);
 				if(!m.find()) {
 					System.out.println("支店定義ファイルのフォーマットが不正です");
+					return;
 				}
 			}
 			br.close();
@@ -76,6 +85,7 @@ public class UriageSyukei {
 		        Matcher m = p.matcher(item[0]);
 				if(!m.find()) {
 					System.out.println("商品定義ファイルのフォーマットが不正です");
+					return;
 				}
 			}
 			br.close();
@@ -117,12 +127,17 @@ public class UriageSyukei {
 				while ((line = br.readLine()) != null) {  //文字列データの受け取り
 					contents.add(line);
 				}
-//				System.out.println(contents.get(0)); //支店番号
+//				System.out.println(contents.get(0)); //支店コード
 //				System.out.println(contents.get(1)); //商品コード
 //				System.out.println(contents.get(2)); //売上額
 
+				if(!store.containsKey(contents.get(0))) { //storeマップにリスト「contents」と同じキーが含まれているか
+					System.out.println("<該当ファイル名>の支店コードが不正です"); //falseなら左文章を表示
+					return;
+				}
+
 				long sale = Long.parseLong(contents.get(2)); //売上げ額をLong型の数値に
-				long shop = brasum.get(contents.get(0)); //支店番号をgetする
+				long shop = brasum.get(contents.get(0)); //支店コードをgetする
 				long code = comsum.get(contents.get(1)); //商品コードをgetする
 //				System.out.println(contents.get(0));
 //				System.out.println(sale + shop);
@@ -138,13 +153,9 @@ public class UriageSyukei {
 					System.out.println("合計金額が10桁を超えました");
 					return;
 				}
-
-//				String shopM = Long.toString(shop);
-//				if(Pattern.compile(shopM).matcher().matches()) {
-//				}
-
 				if(!(contents.size() == 3)) { //要素数が3と同じではない場合のみ、以下のメッセージを表示
-				System.out.println("＜該当ファイル名＞のフォーマットが不正です");
+					System.out.println("＜該当ファイル名＞のフォーマットが不正です");
+					return;
 				}
 
 				br.close();
@@ -157,7 +168,56 @@ public class UriageSyukei {
 				System.out.println("商品定義ファイルのフォーマットが不正です");
 				return;
 		}
-//		System.out.println(brasum.entrySet());
-//		System.out.println(comsum.entrySet());
+
+//4.集計結果出力
+	//支店別集計ファイル
+		File branch = new File(args[0],"\\branch.out");
+		try {
+			FileWriter fw = new FileWriter(branch);
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			List<Map.Entry<String, Long>> shopSum =
+					new ArrayList<Map.Entry<String, Long>>(brasum.entrySet());// List生成、ソートここから
+			Collections.sort(shopSum, new Comparator<Map.Entry<String,Long>>() {
+
+				@Override
+				public int compare(Entry<String,Long> entry1, Entry<String,Long> entry2) { //compareを使って比較
+					return ((Long)entry2.getValue()).compareTo((Long)entry1.getValue());
+				}
+			});
+			for(Entry<String, Long> s : shopSum) {
+				bw.write(s.getKey() + "," + store.get(s.getKey()) + "," + s.getValue());
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+	//商品別集計ファイル
+		File commodity = new File(args[0], "\\commodity.out");
+		try {
+			FileWriter fw = new FileWriter(commodity);
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			List<Map.Entry<String, Long>> codeSum =
+					new ArrayList<Map.Entry<String, Long>>(comsum.entrySet());// List生成、ソートここから
+			Collections.sort(codeSum, new Comparator<Map.Entry<String,Long>>() {
+
+				@Override
+				public int compare(Entry<String,Long> entry1, Entry<String,Long> entry2) { //compareを使って比較
+					return ((Long)entry2.getValue()).compareTo((Long)entry1.getValue());
+				}
+			});
+			for(Entry<String, Long> t : codeSum) {
+				bw.write(t.getKey() + "," + products.get(t.getKey()) + "," + t.getValue());
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+		} catch(IOException e) {
+			System.out.println(e);
+		}
 	}
 }
